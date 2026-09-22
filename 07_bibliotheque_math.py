df957b04fa32116d0c1af199e0c7fad89642c6fc
souky17 <88511228+souky17@users.noreply.github.com>
2026-09-22 09:26:05 -0400

    Delete 07_bibliotheque_math.py

--- 07_bibliotheque_math.py
+++
@@ -1,9 +0,0 @@
-# 07_bibliotheque_math.py
-# Objectif : utiliser une bibliothèque standard
-
-import math
-
-rayon = float(input("Rayon : "))
-aire = math.pi * rayon ** 2
-
-print("Aire :", round(aire, 2))
